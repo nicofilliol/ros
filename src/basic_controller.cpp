b69810a3adde20f005abf60fb4e0f54dd4080256
nicofilliol <nicolas.filliol@outlook.com>
2019-07-31 12:20:56 +0100

    basic node structure

--- src/basic_controller.cpp
+++ src/basic_controller.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
 
-int main()
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
+
+
+class TurtleController
 {
-    std::cout << "Hello RobotCraft!" << std::endl;
+private:
+    ros::NodeHandle n;
+    ros::Publisher cmd_vel_pub;
+
+    geometry_msgs::Twist calculateCommand()
+    {
+        auto msg = geometry_msgs::Twist();
+        
+        // TODO: Control code goes here
+
+        return msg;
+    }
+
+public:
+    TurtleController(){
+        // Initialize ROS
+        this->n = ros::NodeHandle();
+
+        // Create a publisher object, able to push messages
+        // TODO
+    }
+
+    void run(){
+        // Send messages in a loop
+        ros::Rate loop_rate(10);
+        while (ros::ok())
+        {
+            // Calculate the command to apply
+            auto msg = calculateCommand();
+
+            // Publish the new command
+            // TODO
+
+            // And throttle the loop
+            loop_rate.sleep();
+        }
+    }
+
+};
+
+
+int main(int argc, char **argv){
+    // Initialize ROS
+    ros::init(argc, argv, "talker");
+
+    // Create our controller object and run it
+    auto controller = TurtleController();
+    controller.run();
+
+    // And make good on our promise
     return 0;
 }
